0e3ab185de66d6cd5c102eff86deec4708a6b30c
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-01-16 13:30:11 +0200

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', cardGallery(galleryItems))
+
+function cardGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class= "gallery__item">
+     <a class = "gallery__link" href = "${original}"/>
+             <img class = "gallery__image"
+            src = "${preview}"           
+            data-source = "${original}"
+            alt = "${description}" />
+        </a>
+      </div>`;
+    }).join('');  
+}
+
+
+galleryEl.addEventListener('click', onClickImage)
+
+function onClickImage(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+       return
+    }
+    const onImageModal = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="auto" >
+`)
+    onImageModal.show()
+
+    galleryEl.addEventListener('keydown',(event) => {
+        if(event.code === 'Escape'){
+        onImageModal.close()
+        }
+    })
+ 
+}
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('afterbegin', cardGallery(galleryItems))
+
+function cardGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class = "gallery__item" href = "${original}"/>
+             <img class = "gallery__image"
+            src = "${preview}"           
+            alt = "${description}" />
+        </a>
+      </div>`;
+    }).join('');  
+}
+galleryEl.addEventListener('click', onImageClick)
+function onImageClick(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+const lightbox  = new SimpleLightbox('a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+})
+    
